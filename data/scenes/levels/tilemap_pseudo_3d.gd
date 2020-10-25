3f7fbef93d79e912cf7e3ac394bf50f0bdae3aa2
Hugo Locurcio <hugo.locurcio@hugo.pro>
2020-10-25 14:35:58 +0100

    Make the pseudo-3D tilemap effect previewable in the editor

--- data/scenes/levels/tilemap_pseudo_3d.gd
+++ data/scenes/levels/tilemap_pseudo_3d.gd
@@ -1,6 +1,7 @@
 # Copyright © 2016-2020 Hugo Locurcio and contributors - MIT License
 # See `LICENSE.md` included in the source distribution for details.
 
+tool
 extends TileMap
 
 # Higher number of iterations looks better, but is slower.
