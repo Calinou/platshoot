842c2d53830903d6acde848e1a1bb0c20079d613
Hugo Locurcio <hugo.locurcio@hugo.pro>
2020-10-25 16:28:54 +0100

    Always start a dedicated server if using a headless server
    A headless server binary can't be used as a client, so a listen
    server doesn't make sense.

--- data/scenes/menu/menu.gd
+++ data/scenes/menu/menu.gd
@@ -8,21 +8,23 @@ onready var multiplayer_lobby := $MultiplayerLobby as Control
 
 
 func _ready() -> void:
-	if "--server" in OS.get_cmdline_args():
-		# "Listen" server (run from a player client).
-		main.visible = false
-		multiplayer_lobby.start_server()
-		# Don't allow using both `--server` and `--client` at the same time.
-		return
-
-	if "--dedicated" in OS.get_cmdline_args():
+	if OS.has_feature("Server") and "--server" in OS.get_cmdline_args() or "--dedicated" in OS.get_cmdline_args():
+		print("CLI: Starting dedicated server...")
 		# "Dedicated" server (run from a non-player client).
 		main.visible = false
 		multiplayer_lobby.dedicated = true
 		multiplayer_lobby.start_server()
 		return
 
+	if "--server" in OS.get_cmdline_args():
+		print('CLI: Starting "listen" server...')
+		# "Listen" server (run from a player client).
+		main.visible = false
+		multiplayer_lobby.start_server()
+		return
+
 	if "--client" in OS.get_cmdline_args():
+		print("CLI: Joining multiplayer server...")
 		main.visible = false
 		multiplayer_lobby.join_server()
 		return
